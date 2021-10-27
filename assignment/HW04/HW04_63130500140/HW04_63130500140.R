8fdfb83d11a628fed3bba27071f8e93b9f584dc8
mottdy <68776714+mottdy@users.noreply.github.com>
2021-10-27 23:58:07 +0700

    Update HW04_63130500140.R

--- assignment/HW04/HW04_63130500140/HW04_63130500140.R
+++ assignment/HW04/HW04_63130500140/HW04_63130500140.R
@@ -10,6 +10,19 @@ books <- read.csv("https://raw.githubusercontent.com/safesit23/INT214-Statistics
 # viewdataset
 glimpse(books)
 
+# learn tidyverse
+books %>% select(Book_title, Type)
+books %>% filter(Type == "Paperback")
+books %>% arrange(Price)
+books %>% arrange(desc(Price))
+books %>% select(Type) %>% group_by(Type)
+books %>% group_by(Type) %>% count()
+books %>% group_by(Type)
+books %>% mutate(vat = Price*0.07)
+books %>% distinct(Type)
+
+# insight
+
 books %>% group_by(Type) %>% count()
 
 books %>% filter(Reviews==0) %>% select(Book_title,Type)  
@@ -22,14 +35,12 @@ books %>% select(Book_title,Price) %>% arrange(desc(Price)) %>% head(n = 10L)
 
 books %>% filter(Rating > mean(Rating, na.rm = TRUE)) %>% group_by(Type) %>% count()
 
-
+# graph
 graph<- ggplot(books,aes(x=Type)) + geom_bar()
 graph
 graph + ggtitle("Graph show the number of book in each type") + xlab("Type") + ylab("number of book")
 
 
-graph2 <- books %>% filter(Number_Of_Pages<1500) %>% ggplot(aes(x=Number_Of_Pages,y=Price))+ geom_point() +
-  geom_point(aes(color=Type))+ ggtitle("relation between Number Of pages and Price") + geom_smooth() 
+graph2 <- books %>% filter(Number_Of_Pages<1500) %>% ggplot(aes(x=Number_Of_Pages,y=Price))+
+  geom_point(aes(color=Type))+ ggtitle("relation between Number Of pages and Price")
 graph2
-
-
